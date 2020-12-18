8131219f7d015e62946fd7519b400b2fd3946199
jntakpe <jntakpe@gmail.com>
2020-12-18 22:18:18 +0100

    build: init project

---
+++ build.gradle.kts
@@ -0,0 +1,91 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val commonsVersion: String by project
+val springCloudVersion: String by project
+
+plugins {
+    idea
+    jacoco
+    id("org.springframework.boot") version "2.4.0"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("com.google.cloud.tools.jib") version "2.7.0"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
+}
+
+group = "com.github.jntakpe"
+version = "0.0.1"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+    mavenGithub("equidis/sb-commons")
+    maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+extra["springCloudVersion"] = springCloudVersion
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+    implementation("com.github.jntakpe:sb-commons-cache:$commonsVersion")
+    implementation("com.github.jntakpe:sb-commons-mongo:$commonsVersion")
+    implementation("com.github.jntakpe:sb-commons-web:$commonsVersion")
+    testImplementation("com.github.jntakpe:sb-commons-cache-test:$commonsVersion")
+    testImplementation("com.github.jntakpe:sb-commons-mongo-test:$commonsVersion")
+    testImplementation("com.github.jntakpe:sb-commons-test:$commonsVersion")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+jib {
+    to {
+        image = "eu.gcr.io/equidis/springboot-availability:${project.version}"
+    }
+}
+
+tasks {
+    jacocoTestReport {
+        dependsOn(test)
+        reports {
+            xml.isEnabled = true
+        }
+        classDirectories.setFrom(
+            sourceSets.main.get().output.asFileTree.matching {
+                exclude("build/generated", "**/model/entity/**")
+            }
+        )
+    }
+    check {
+        dependsOn(jacocoTestReport)
+    }
+}
+
+fun RepositoryHandler.mavenGithub(repository: String) = maven {
+    name = "Github_packages"
+    setUrl("https://maven.pkg.github.com/$repository")
+    credentials {
+        val githubActor: String? by project
+        val githubToken: String? by project
+        username = githubActor
+        password = githubToken
+    }
+}
+
