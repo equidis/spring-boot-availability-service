c073a450a08a4a1a6c5581ce49062eff7fc7334d
jntakpe <jntakpe@gmail.com>
2020-12-23 18:53:58 +0100

    fix: remove unnecessary create method in UserAvailabilityRepository.kt since it's already added by Spring Data

--- src/main/kotlin/com/github/jntakpe/sbavailability/repository/UserAvailabilityRepository.kt
+++ src/main/kotlin/com/github/jntakpe/sbavailability/repository/UserAvailabilityRepository.kt
@@ -3,11 +3,8 @@ package com.github.jntakpe.sbavailability.repository
 import com.github.jntakpe.commons.mongo.SbReactiveMongoRepository
 import com.github.jntakpe.sbavailability.model.entity.UserAvailability
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
 interface UserAvailabilityRepository : SbReactiveMongoRepository<UserAvailability> {
 
     fun findByUserId(userId: String): Flux<UserAvailability>
-
-    fun create(availability: UserAvailability): Mono<UserAvailability>
 }
