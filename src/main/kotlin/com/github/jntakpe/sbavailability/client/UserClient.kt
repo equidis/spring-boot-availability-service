5d93300a52e238c09b44f1a0be11c075cad5f163
jntakpe <jntakpe@gmail.com>
2020-12-24 12:10:20 +0100

    feat: implement client exception mappings

--- src/main/kotlin/com/github/jntakpe/sbavailability/client/UserClient.kt
+++ src/main/kotlin/com/github/jntakpe/sbavailability/client/UserClient.kt
@@ -1,22 +1,49 @@
 package com.github.jntakpe.sbavailability.client
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.jntakpe.commons.context.CommonException
+import com.github.jntakpe.commons.context.CommonExceptionDto
+import com.github.jntakpe.commons.context.logger
+import com.github.jntakpe.commons.context.toException
 import com.github.jntakpe.sbavailability.model.dto.client.UserClientDto
 import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
 
 @Component
 class UserClient(discoveryClient: DiscoveryClient) {
 
+    private val log = logger()
+    private val objectMapper = jacksonObjectMapper()
     private val client = WebClient.builder().baseUrl("${discoveryClient.getInstances("sb-users").first().uri}/users").build()
 
-    fun findById(id: String): Mono<UserClientDto> = client.get().uri("/{id}", id).retrieve().bodyToMono(UserClientDto::class.java)
+    fun findById(id: String): Mono<UserClientDto> {
+        return client.get()
+            .uri("/{id}", id)
+            .retrieve()
+            .bodyToMono(UserClientDto::class.java)
+            .onErrorMap { it.toCommonException() }
+    }
 
     fun findByUsername(username: String): Mono<UserClientDto> {
         return client.get()
             .uri { it.queryParam("username", username).build() }
             .retrieve()
             .bodyToMono(UserClientDto::class.java)
+            .onErrorMap { it.toCommonException() }
+    }
+
+    private fun Throwable.toCommonException(): CommonException {
+        return (this as? WebClientResponseException)?.toCommonException()
+            ?: CommonException("Unexpected client error", log::warn, HttpStatus.INTERNAL_SERVER_ERROR, this)
+    }
+
+    private fun WebClientResponseException.toCommonException(): CommonException {
+        return objectMapper.readValue<CommonExceptionDto>(responseBodyAsByteArray)
+            .toException(log::info)
     }
 }
--- src/test/kotlin/com/github/jntakpe/sbavailability/client/UserClientTest.kt
+++ src/test/kotlin/com/github/jntakpe/sbavailability/client/UserClientTest.kt
@@ -1,9 +1,11 @@
 package com.github.jntakpe.sbavailability.client
 
+import com.github.jntakpe.commons.context.CommonException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
 import reactor.kotlin.test.test
 
 @SpringBootTest
@@ -15,5 +17,37 @@ internal class UserClientTest(@Autowired private val client: UserClient) {
         client.findById(id).test()
             .consumeNextWith { assertThat(it.id).isEqualTo(id) }
             .verifyComplete()
+    }
+
+    @Test
+    fun `find by id should fail when id does not exists`() {
+        val id = "123456789012345678901234"
+        client.findById(id).test()
+            .consumeErrorWith {
+                assertThat(it).isInstanceOf(CommonException::class.java)
+                it as CommonException
+                assertThat(it.status).isEqualTo(HttpStatus.NOT_FOUND)
+            }
+            .verify()
+    }
+
+    @Test
+    fun `find by username should return user`() {
+        val username = "jdoe"
+        client.findByUsername(username).test()
+            .consumeNextWith { assertThat(it.username).isEqualTo(username) }
+            .verifyComplete()
+    }
+
+    @Test
+    fun `find by username should fail when username does not exists`() {
+        val username = "unknown"
+        client.findByUsername(username).test()
+            .consumeErrorWith {
+                assertThat(it).isInstanceOf(CommonException::class.java)
+                it as CommonException
+                assertThat(it.status).isEqualTo(HttpStatus.NOT_FOUND)
+            }
+            .verify()
     }
 }
